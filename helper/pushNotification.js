e7dc57b1ba34b9ddce2a2162cd3bd8279628f30b
dheeraj071523 <118386811+dheeraj071523@users.noreply.github.com>
2025-01-23 16:30:39 +0530

    Update pushNotification.js

--- helper/pushNotification.js
+++ helper/pushNotification.js
@@ -5,43 +5,22 @@ import { ID } from "appwrite";
 
 export const requestNotificationPermission = async () => {
   if ("Notification" in window) {
-    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    const permission = await Notification.requestPermission();
 
-    if (isSafari) {
-      console.log("Safari browser detected.");
-      
-      if (Notification.permission === "granted") {
-        console.log("Notification permission already granted in Safari.");
-        const token = await getToken(messaging, { vapidKey: VAPID_KEY });
-        console.log("+++ token", token);
-        await databases.createDocument(
-          process.env.NEXT_PUBLIC_DATABASE_ID,
-          process.env.NEXT_PUBLIC_COLLECTION_ID,
-          ID.unique(),
-          { token: token }
-        );
-        console.log("Token saved in database.");
-      } else {
-        alert("Safari does not allow manual notification permission prompts.");
-      }
-    } else {
-      const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      console.log("Notification permission granted.");
 
-      if (permission === "granted") {
-        console.log("Notification permission granted.");
-
-        const token = await getToken(messaging, { vapidKey: VAPID_KEY });
-        console.log("+++ token", token);
-        await databases.createDocument(
-          process.env.NEXT_PUBLIC_DATABASE_ID,
-          process.env.NEXT_PUBLIC_COLLECTION_ID,
-          ID.unique(),
-          { token: token }
-        );
-        console.log("Token saved in database.");
-      } else if (permission === "denied") {
-        alert('Permission denied for notifications.');
-      }
+      const token = await getToken(messaging, { vapidKey: VAPID_KEY });
+      console.log("+++ token", token);
+      await databases.createDocument(
+        process.env.NEXT_PUBLIC_DATABASE_ID,
+        process.env.NEXT_PUBLIC_COLLECTION_ID,
+        ID.unique(),
+        { token: token }
+      );
+      console.log("token saved in database");
+    } else if (permission === "denied") {
+      alert('permission === "denied"');
     }
   }
 };
